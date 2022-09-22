368e4b1f30d4b38efeda56933d5605d9f6a955b1
flavio.santana <9090flavio@gmail.com>
2022-09-22 16:02:52 -0300

    desv: recebendo e devolvendo objetos JSON

--- src/main/java/io/udemy/quarkus/resource/UsuariosResource.java
+++ src/main/java/io/udemy/quarkus/resource/UsuariosResource.java
@@ -2,16 +2,24 @@ package io.udemy.quarkus.resource;
 
 import io.udemy.quarkus.dto.UsuarioDto;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/usuarios")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UsuariosResource {
 
     @POST
     public Response criarUsuario(UsuarioDto usuarioDto) {
-        return Response.status(Response.Status.CREATED).build();
+        return Response.ok(usuarioDto).build();
     }
+
+    @GET
+    public Response listarTodosUsuarios() {
+        return Response.ok().build();
+    }
+
 
 }
