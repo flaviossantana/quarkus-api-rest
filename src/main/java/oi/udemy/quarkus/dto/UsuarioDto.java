c304c6b5676592eeef8cbc2670ee32f3b7b89853
flavio.santana <9090flavio@gmail.com>
2022-09-22 14:26:58 -0300

    desv: criando o post de users

---
+++ src/main/java/oi/udemy/quarkus/dto/UsuarioDto.java
@@ -0,0 +1,23 @@
+package oi.udemy.quarkus.dto;
+
+public class UsuarioDto {
+
+    private String nome;
+    private String idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+}
---
+++ src/main/java/oi/udemy/quarkus/resource/UsuariosResource.java
@@ -0,0 +1,17 @@
+package oi.udemy.quarkus.resource;
+
+import oi.udemy.quarkus.dto.UsuarioDto;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/usuarios")
+public class UsuariosResource {
+
+    @POST
+    public Response criarUsuario(UsuarioDto usuarioDto) {
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+}
