5c5b024624e931d9660e1a114643dd3cc06afb74
flavio.santana <9090flavio@gmail.com>
2022-09-26 10:06:21 -0300

    desv: Concluindo as operações CRUD

--- src/main/java/io/udemy/quarkus/resource/UsuariosResource.java
+++ src/main/java/io/udemy/quarkus/resource/UsuariosResource.java
@@ -7,11 +7,17 @@ import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Optional;
 
 @Path("/usuarios")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UsuariosResource {
+
+    @GET
+    public Response listarTodosUsuarios() {
+        return Response.ok(Usuario.listAll()).build();
+    }
 
     @POST
     @Transactional
@@ -23,9 +29,37 @@ public class UsuariosResource {
         return Response.ok(usuario).build();
     }
 
-    @GET
-    public Response listarTodosUsuarios() {
-        return Response.ok(Usuario.listAll()).build();
+    @DELETE
+    @Transactional
+    @Path("{id}")
+    public Response excluirUsuario(@PathParam("id") Long id) {
+
+        Optional<Usuario> usuarioOptional = Usuario.findByIdOptional(id);
+
+        if(usuarioOptional.isEmpty()) {
+            return Response.status(Response.Status.NO_CONTENT).build();
+        }
+
+        usuarioOptional.get().delete();
+        return Response.ok().build();
+
+    }
+
+    @PUT
+    @Transactional
+    @Path("{id}")
+    public Response atualizarUsuario(@PathParam("id") Long id, UsuarioDto dto) {
+        Optional<Usuario> usuarioOptional = Usuario.findByIdOptional(id);
+
+        if(usuarioOptional.isEmpty()) {
+            return Response.status(Response.Status.NO_CONTENT).build();
+        }
+
+        Usuario usuario = usuarioOptional.get();
+        usuario.setNome(dto.getNome());
+        usuario.setIdade(dto.getIdade());
+
+        return Response.ok().build();
     }
 
 }
