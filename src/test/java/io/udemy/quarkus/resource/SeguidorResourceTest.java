4afe31c65c3d746b71adf29915fba4e6477b0ebe
flavio.santana <9090flavio@gmail.com>
2022-09-28 18:18:23 -0300

    desv: Testes na API de Followers

---
+++ src/test/java/io/udemy/quarkus/resource/SeguidorResourceTest.java
@@ -0,0 +1,79 @@
+package io.udemy.quarkus.resource;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import io.udemy.quarkus.dto.SeguidorRequestDto;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+@TestHTTPEndpoint(SeguidorResource.class)
+class SeguidorResourceTest {
+
+    Long idUsuarioInexistente = 999999999L;
+
+    @Test
+    @DisplayName("Tentar Seguir ele mesmo")
+    public void testSeguirEleMesmo() {
+                given()
+                        .contentType(ContentType.JSON)
+                        .pathParams("userId", 100)
+                        .body(new SeguidorRequestDto(100l))
+                        .put()
+                        .then()
+                        .statusCode(HttpStatus.SC_CONFLICT);
+    }
+
+    @Test
+    @DisplayName("Seguidor ou Usuario não encontrado")
+    public void naoEncontrouSeguidorOuUSuario() {
+        given()
+                .contentType(ContentType.JSON)
+                .pathParams("userId", 1000)
+                .body(new SeguidorRequestDto(2000l))
+                .put()
+                .then()
+                .statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("Seguir Usuario")
+    public void testSeguirUsuario() {
+        given()
+                .contentType(ContentType.JSON)
+                .pathParams("userId", 100)
+                .body(new SeguidorRequestDto(200l))
+                .put()
+                .then()
+                .statusCode(HttpStatus.SC_NO_CONTENT);
+    }
+
+    @Test
+    @DisplayName("Seguidor usuário inexistente")
+    public void testeDeixarDeSeguirUsuarioInexistente(){
+        given()
+                .contentType(ContentType.JSON)
+                .pathParams("userId", idUsuarioInexistente)
+                .body(new SeguidorRequestDto(200l))
+                .delete()
+                .then()
+                .statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("Deixar de seguir usuário")
+    public void deixarDeSeguirUsuario(){
+        given()
+                .contentType(ContentType.JSON)
+                .pathParams("userId", 100)
+                .body(new SeguidorRequestDto(200l))
+                .delete()
+                .then()
+                .statusCode(HttpStatus.SC_NO_CONTENT);
+    }
+
+}
