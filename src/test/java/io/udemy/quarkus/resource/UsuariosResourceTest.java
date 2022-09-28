6b3fc7153750f85ac343b361b9e262ab5c015efe
flavio.santana <9090flavio@gmail.com>
2022-09-28 15:42:37 -0300

    desv: Testando o caso de erro de validação

--- src/test/java/io/udemy/quarkus/resource/UsuariosResourceTest.java
+++ src/test/java/io/udemy/quarkus/resource/UsuariosResourceTest.java
@@ -3,11 +3,12 @@ package io.udemy.quarkus.resource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.udemy.quarkus.dto.UsuarioDto;
+import io.udemy.quarkus.validator.ResponseError;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
 
 
 @QuarkusTest
@@ -26,6 +27,21 @@ class UsuariosResourceTest {
                 .body("nome", is("João Biosco"))
                 .body("idade", is(30));
 
+    }
+
+    @Test
+    @DisplayName("Criar um usuário: STATUS 422")
+    public void criarUsuarioComNomeNulo() {
+        given()
+                .contentType(ContentType.JSON)
+                .body(new UsuarioDto(null, null))
+                .when()
+                .post("/usuarios")
+                .then()
+                .statusCode(ResponseError.UNPROCESSABLE_ENTITY)
+                .body("message", is("Validation error"))
+                .body("errors.get(0).get(\"message\")", is("Nome é obrigatório"))
+                .body("errors.get(1).get(\"message\")", is("Idade é obrigatório"));
     }
 
 }
